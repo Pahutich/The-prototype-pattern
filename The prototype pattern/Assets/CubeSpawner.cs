892dabd68ce2b914ed03ecb68a5047d330f0f63a
Pahutich <dima.gamedev@pm.me>
2023-08-13 20:23:00 +0200

    The prototype pattern

---
+++ The prototype pattern/Assets/CubeSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0, 100) < 10)
+            ProcCube.CreateCube(transform.position);
+    }
+}
